c804ca24ff3c156463263751c881902559b3dbcb
Varun Maharaj <varunmaharaj031@gmail.com>
2021-12-02 15:03:22 +0200

    chore: registered DB Context Service

---
+++ ConferencePlanner/Database/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using ConferencePlanner.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConferencePlanner.Database
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+}
